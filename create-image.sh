edfb8eaa6d6cadd95f2ceedbb2514559465fd138
David Loup <david.loup@inria.fr>
2017-03-30 13:33:27 -0500

    Fix typo execution rigths on heat-config-source

--- create-image.sh
+++ create-image.sh
@@ -23,8 +23,10 @@ fi
 if [ ! -d heat-templates ]; then
   git clone https://git.openstack.org/openstack/heat-templates.git
 fi
-#Fix : Install Heat agent in virtualenv to not interfere with Openstack packages
+#Fix : Install Heat agent in virtualenv to not interfere with Openstack packages and fix typo
+rm python_fix heat-templates/hot/software-config/elements/heat-config/install.d/heat-config-source-install/50-heat-config-soure
 cp python_fix heat-templates/hot/software-config/elements/heat-config/install.d/heat-config-source-install/50-heat-config-source
+chmod 775 heat-templates/hot/software-config/elements/heat-config/install.d/heat-config-source-install/50-heat-config-source
 
 URL_ROOT="https://cloud-images.ubuntu.com/releases/$UBUNTU_VERSION/$BUILD_DATE"
 if [ ! -f "$BASE_IMAGE" ]; then
